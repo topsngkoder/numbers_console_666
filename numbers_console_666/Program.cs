379d4138f33eecff5d860c4d3cf4c919b716c91e
Dimka <mir24zvuk@yandex.ru>
2023-06-05 18:02:26 +0300

    Create new project

---
+++ numbers_console_666/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace numbers_console_666
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
